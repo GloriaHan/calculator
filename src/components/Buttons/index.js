27ed06571b3b0437daff97ffb0f3a0d061272b85
Gloria <hanning_hit@hotmail.com>
2022-09-12 13:14:45 +1000

    first commit

---
+++ src/components/Buttons/index.js
@@ -0,0 +1,13 @@
+import React from 'react'
+
+export default function Button() {
+  return (
+    <div>
+        <button>1</button>
+        <button>2</button>
+        <button>3</button>
+        <button>4</button>
+
+    </div>
+  )
+}
---
+++ src/components/Calculator/index.js
@@ -0,0 +1,10 @@
+import React from 'react'
+import Button from '../Buttons'
+
+export default function Calculator() {
+  return (
+    <div>
+      <Button/>
+    </div>
+  )
+}
